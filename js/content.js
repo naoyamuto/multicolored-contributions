d8ee5194f0992b83bb52aa9de11241c93c140f66
naoyamuto <naoya.mutou@gmail.com>
2017-02-21 01:08:29 +0900

    Cosmetic

--- js/content.js
+++ js/content.js
@@ -1,45 +1,45 @@
- 'use strict';
+'use strict';
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     changeColor(getColorArray(msg.rpc));
-  });
+});
 
 function changeColor(colors) {
-  var calenderGraphSvg = document.querySelector(".js-calendar-graph-svg");
-	var gElements = calenderGraphSvg.querySelector("g").getElementsByTagName("g");
-	var legendElements = document.querySelector(".legend").getElementsByTagName("li");
+    var calenderGraphSvg = document.querySelector(".js-calendar-graph-svg");
+    var gElements = calenderGraphSvg.querySelector("g").getElementsByTagName("g");
+    var legendElements = document.querySelector(".legend").getElementsByTagName("li");
 
-	[].forEach.call(gElements, function(calenderGraphSvg) {
-		var rectElements = calenderGraphSvg.getElementsByTagName("rect");
+    [].forEach.call(gElements, function(calenderGraphSvg) {
+        var rectElements = calenderGraphSvg.getElementsByTagName("rect");
 
-		[].forEach.call(rectElements, function(rectElement){
-			var fill = rectElement.getAttribute("fill");
+        [].forEach.call(rectElements, function(rectElement) {
+            var fill = rectElement.getAttribute("fill");
 
-			switch(fill) {
-				case "#d6e685":
-            rectElement.setAttribute("fill", colors[0]);
-            break;
-        case "#8cc665":
-            rectElement.setAttribute("fill", colors[1]);
-            break;
-        case "#44a340":
-            rectElement.setAttribute("fill", colors[2]);
-            break;
-        case "#1e6823":
-            rectElement.setAttribute("fill", colors[3]);
-            break;
-        default:
-            break;
-			}
-		});
-	});
+            switch (fill) {
+                case "#d6e685":
+                    rectElement.setAttribute("fill", colors[0]);
+                    break;
+                case "#8cc665":
+                    rectElement.setAttribute("fill", colors[1]);
+                    break;
+                case "#44a340":
+                    rectElement.setAttribute("fill", colors[2]);
+                    break;
+                case "#1e6823":
+                    rectElement.setAttribute("fill", colors[3]);
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
 
-	[].forEach.call(legendElements, function(legendElement){
-		var style = legendElement.style;
-		var legendElementColor = style.backgroundColor;
+    [].forEach.call(legendElements, function(legendElement) {
+        var style = legendElement.style;
+        var legendElementColor = style.backgroundColor;
 
-		switch(legendElementColor) {
-			case "rgb(214, 230, 133)":
+        switch (legendElementColor) {
+            case "rgb(214, 230, 133)":
                 style.backgroundColor = colors[0];
                 break;
             case "rgb(140, 198, 101)":
@@ -53,8 +53,8 @@ function changeColor(colors) {
                 break;
             default:
                 break;
-		}
-	});
+        }
+    });
 }
 
 function getColorArray(colorId) {
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function onClick(e) {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    var className = e.target.className;
-    chrome.tabs.sendMessage(tabs[0].id, {rpc: className}, (msg) => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        var className = e.target.className;
+        chrome.tabs.sendMessage(tabs[0].id, {rpc: className}, (msg) => {
+        });
     });
-  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var lists = document.getElementsByTagName("li");
-  for (var i = 0; i < lists.length; i++) {
-    lists[i].addEventListener('click', onClick , false);
-  }
+    var lists = document.getElementsByTagName("li");
+    for (var i = 0; i < lists.length; i++) {
+        lists[i].addEventListener('click', onClick , false);
+    }
 });
